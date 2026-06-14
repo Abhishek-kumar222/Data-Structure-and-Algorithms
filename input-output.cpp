368c76693717e2a3949ce99b592f1d0d3ecb1a3d
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-06-14 16:51:46 +0530

    code

--- input-output.cpp
+++ input-output.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main() {
    int marks[5] = {1,4,2,5,2,};
    int len = (sizeof(marks)/sizeof(int)); // ab arr ka sahi size milega(lenth)
-
+ cout<<"-----input-----"<<endl;
+ for(int i=0;i<len;i++){
+    cin>>marks[i];
+   }
+    
+ cout<<"-----output-----"<<endl;
    for(int i=0;i<len;i++){
     cout<<marks[i]<<",";
    }
